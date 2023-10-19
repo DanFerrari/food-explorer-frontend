62a348ade0944c623facb4f5f060aefdefae6c88
DanFerrari <danieldasilvaferrari@gmail.com>
2023-10-18 23:03:46 -0300

    Inserido cores e fonts no assets

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+        <GlobalStyles>
+            <AuthProvider>
+                  <Routes/>
+            </AuthProvider>
+        </GlobalStyles>  
+    </ThemeProvider> 
+    
   </React.StrictMode>,
 )
